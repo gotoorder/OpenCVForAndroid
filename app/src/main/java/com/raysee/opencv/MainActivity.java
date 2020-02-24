ecc14da20afebffe7bbdaee7b00dae4f23dcb169
idoes <idoes@msn.com>
2020-02-24 16:56:21 +0800

    Initial commit

---
+++ app/src/main/java/com/raysee/opencv/MainActivity.java
@@ -0,0 +1,16 @@
+package com.raysee.opencv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
